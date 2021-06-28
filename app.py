9184e4bad94700a7395148afb9ddb4dc0bc40f99
nachovera93 <ricardoignaciovera.93@gmail.com>
2021-06-28 17:01:28 -0400

    fase1

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def index():
      return render_template('index.html')
 
+@app.route('/fase1.html')
+def index():
+     return render_template('fase1.html')
+
 
 
 #if __name__ == '__main__': 
