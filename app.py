1de13475cc9a5d945c239419b578f496eedf5275
nachovera93 <ricardoignaciovera.93@gmail.com>
2021-06-28 18:08:57 -0400

    a

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import fase1medidor
 
 app = Flask(__name__)
 
@@ -6,9 +7,9 @@ app = Flask(__name__)
 def index():
      return render_template('index.html')
 
-@app.route('/fase1.html')
-def fase1():
-      return render_template('fase1.html')
+#@app.route('/fase1.html')
+#def fase1():
+ #     return render_template('fase1.html')
 
 
 
--- fase1medidor.py
+++ fase1medidor.py
@@ -2,9 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-     return render_template('index.html')
+#@app.route('/')
+#def index():
+ #    return render_template('index.html')
 
 @app.route('/fase1.html')
 def fase1():
